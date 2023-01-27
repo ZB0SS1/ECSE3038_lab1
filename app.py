80e8e7a015d4d6d920af3200f00aa285d4389918
ZboSS <gallimoreraheim@gmail.com>
2023-01-27 14:54:42 -0500

    removed brackets

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ def potential_divider(volt , list):
 
 
 def temperature_check(temp,unit):
-    if (unit == "F" or unit == "f"):
+    if unit == "F" or unit == "f":
         unit = "C"
-        temp =  (temp - 32)*5/9
+        temp = (temp - 32)*5/9
     
-    if (unit == "C" or unit == "c"):
-        if (temp < 30):
+    if unit == "C" or unit == "c":
+        if temp <= 30:
             print("The patient is hypothermic")
-        elif (temp > 40):
+        elif temp >= 40:
             print("the patient is hyperthermis")
-        elif(temp >= 30 and temp <= 40):
+        elif temp > 30 and temp < 40:
             print("The patient is normal")
         else:
             print("Enter a usable number")
@@ -50,3 +50,5 @@ def temperature_check(temp,unit):
 paralell([332,1000,2200])
 
 potential_divider(9,[3000,1000])
+
+temperature_check(15,"f")
