c8a50719604bf6a5a9d1f6ff5be1a8a5093cfa25
ZboSS <gallimoreraheim@gmail.com>
2023-01-27 14:39:22 -0500

    wrote a code for temperature check

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def paralell (list):
 
 
 def potential_divider(volt , list):
-    #return volt(list[1]/(list[0]+list[1]))
     y = 0
     for x in list:
         y = y + x
@@ -23,9 +22,31 @@ def potential_divider(volt , list):
         print(z,"v") 
 
 
+def temperature_check(temp,unit):
+    if (unit == "F" or unit == "f"):
+        unit = "C"
+        temp =  (temp - 32)*5/9
+    
+    if (unit == "C" or unit == "c"):
+        if (temp < 30):
+            print("The patient is hypothermic")
+        elif (temp > 40):
+            print("the patient is hyperthermis")
+        elif(temp >= 30 and temp <= 40):
+            print("The patient is normal")
+        else:
+            print("Enter a usable number")
+    else:
+        print("Enter usable unit ")
+
+
+
+
+
+
+
+
 
 paralell([332,1000,2200])
-
-
 
 potential_divider(9,[3000,1000])
